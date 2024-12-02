a19e7ccedeab5f13cc465a647b97b66c0c4c00fe
Ankit <akt.ankitsharma@gmail.com>
2024-12-02 09:01:51 +0530

    New location

--- pain.tf
+++ pain.tf
@@ -2,3 +2,8 @@ resource "azurerm_resource_group" "pawan" {
   name     = "PAnkit"
   location = "East US"
 }
+
+resource "azurerm_resource_group" "pawan" {
+  name     = "PAnkit"
+  location = "West US"
+}
