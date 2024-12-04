7bae5d8efeef8052c5a22a98f23c8834652f671a
Ankit <akt.ankitsharma@gmail.com>
2024-12-04 08:15:36 +0530

    deleted

--- main.tf
+++ main.tf
@@ -2,13 +2,3 @@ resource "azurerm_resource_group" "ankit" {
   name     = "Ankit"
   location = "East US"
 }
-
-resource "azurerm_resource_group" "pankit" {
-  name     = "Pankiy"
-  location = "East US"
-}
-
-resource "azurerm_resource_group" "sankit" {
-  name     = "Sankiy"
-  location = "East US"
-}
