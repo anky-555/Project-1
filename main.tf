1882137abc1b61565aa391b7b432651be9ebbc30
Ankit <akt.ankitsharma@gmail.com>
2025-03-19 23:20:13 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,4 +1,12 @@
-resource "azurerm_resource_group" "ankit" {
+resource "azurerm_resource_group" "ankitrg" {
   name     = "Ankit"
   location = "Central India"
+}
+
+resource "azurerm_storage_account" "ankitsa" {
+name = "ankitsa"
+location = resource_azurerm_resource_group_location
+resource_group_name = resource_azurerm_resource_group_name
+account_replication_type = "LRS"
+account_tier = "Standard"
 }
